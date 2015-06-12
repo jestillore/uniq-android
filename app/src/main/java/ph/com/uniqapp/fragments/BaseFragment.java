6219d40c92edd2bd086287e4fe675e5e7fcae2ae
Jillberth Estillore <jestillore@zoogtech.com>
2015-06-13 01:18:02 +0800

    add getContext method BaseFragment class

--- app/src/main/java/ph/com/uniqapp/fragments/BaseFragment.java
+++ app/src/main/java/ph/com/uniqapp/fragments/BaseFragment.java
@@ -1,9 +1,15 @@
 package ph.com.uniqapp.fragments;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by cerberus on 6/12/15.
  */
 public class BaseFragment extends Fragment {
+
+    public Context getContext() {
+        return getActivity().getApplicationContext();
+    }
+
 }
