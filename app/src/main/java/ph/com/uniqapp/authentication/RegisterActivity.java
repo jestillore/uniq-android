54a63abbd72e77f5262346cb828a8c8b70acd19b
John Manuel <jderecho@zoogtech.com>
2015-06-13 02:24:01 +0800

    added register layouts

---
+++ app/src/main/java/ph/com/uniqapp/authentication/RegisterActivity.java
@@ -0,0 +1,18 @@
+package ph.com.uniqapp.authentication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ph.com.uniqapp.R;
+
+/**
+ * Created by waelhe on 6/12/2015.
+ */
+public class RegisterActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
